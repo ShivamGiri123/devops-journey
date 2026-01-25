4fc1c8525512ac35039c30384c56e77817366d2d
Shivam Giri <Shivamgiri970@gmail.com>
2026-01-25 14:10:12 +0000

    Add server health check script using loops and functions

--- linux/practice/server_Health_check.sh
+++ linux/practice/server_Health_check.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 #simple server health check script
-echo "----server health check report----" 
+
+
+#CPU Usage >> 
+CPU(){
+    echo "CPU Usage:" 
+top -bn1 |grep "Cpu(s)" 
+echo
+}
+
+
+#Memory Usage
+Memory_usage(){
+echo "Memory Usage:" 
+free -h  
+echo
+}
+
+#Disk Usage
+Disk_usage(){
+echo "Disk Usage:" 
+df -h 
+echo
+}
+
+for i in 1 2 3
+do
+echo "----server health check report Run - $i" 
 echo "Date: $(date)" 
 echo 
 
-#CPU Usage >> 
-echo "CPU Usage:" 
-top -bn1 |grep "Cpu(s)" 
-echo 
-
-#Memory Usage
-echo "Memory Usage:" 
-free -h 
-echo 
-
-#Disk Usage
-echo "Disk Usage:" 
-df -h 
-echo 
-
 echo "Report generated successfully!"
+
+CPU
+Memory_usage
+Disk_usage
+
+echo "sleeping for 5 seconds.."
+sleep 5
+done
+
 
